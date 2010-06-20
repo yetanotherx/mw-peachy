89c724dd8ac51c30014f34bc55e0b27c336bb42f
compwhizii <compwhizii@b8eabb53-a4fa-ac67-1dc2-dce8c8e07975>
2010-06-20 13:55:26 +0000

    Updating error message for mediawiki version

--- Init.php
+++ Init.php
@@ -104,7 +104,7 @@ class Peachy {
 		$version = preg_replace( '/[^0-9\.]/','',str_replace('MediaWiki ', '', $siteinfo['query']['general']['generator'] ));
 		
 		if( !version_compare( $version, MINMW ) ) {
-			throw new DependencyError( "MediaWiki 1.12", "http://mediawiki.org" );
+			throw new DependencyError( "MediaWiki " . MINMW, "http://mediawiki.org" );
 		}
 		
 		$mwVersion = $version;

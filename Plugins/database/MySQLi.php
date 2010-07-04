f36da8685a3725924055950a115393cef4e13c3f
soxred93 <soxred93@b8eabb53-a4fa-ac67-1dc2-dce8c8e07975>
2010-07-04 06:43:28 +0000

    Extend MySQLi to MySQL to provide forward-compatibility until the mysqli specific functions are written. (Yay 200 commits!)

--- Plugins/database/MySQLi.php
+++ Plugins/database/MySQLi.php
@@ -20,5 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /**
  * DatabaseMySQLi class, specifies the MySQLi-specific functions 
  */
-class DatabaseMySQLi extends DatabaseBase {
+class DatabaseMySQLi extends DatabaseMySQL {
+	
 }

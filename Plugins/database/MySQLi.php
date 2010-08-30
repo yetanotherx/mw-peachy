c7c9ef2325b7afa42d25649a1167d5eff6139de0
soxred93 <soxred93@b8eabb53-a4fa-ac67-1dc2-dce8c8e07975>
2010-08-30 01:41:24 +0000

    Moar fixes

--- Plugins/database/MySQLi.php
+++ Plugins/database/MySQLi.php
@@ -17,8 +17,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-require_once( $pgIP . 'Plugins/database/MySQL.php' );
-
 /**
  * DatabaseMySQLi class, specifies the MySQLi-specific functions 
  * @package Database
